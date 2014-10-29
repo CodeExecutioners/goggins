a1c61461a62b6c490b01cfe5d619927b6181f069
fisherrm <fisherr@uwec.edu>
2014-10-28 23:20:57 -0500

    Revert "JSON table test"

--- js/admin/lessons.js
+++ js/admin/lessons.js
@@ -17,60 +17,20 @@ var table = $('#lessonsDropIn').DataTable();
     } );
  
     $('#lessonsFormSubmit').on('click', function (event) {
-		event.preventDefault();
-        alert("form submit");
-		var data = JSON.stringify({'vendor': {'name': 'test', 'description': 'a good company', 'tags':['foo', 'bar']}});
-		
-		//iterate through table
-	var columns = $('#myTable thead th').map(function() {
-	  // This assumes that your headings are suitable to be used as
-	  //  JavaScript object keys. If the headings contain characters 
-	  //  that would be invalid, such as spaces or dashes, you should
-	  //  use a regex here to strip those characters out.
-	  return $(this).text();
-		});
-
-		
-		var tableObject = $('#myTable tbody tr').map(function(i) {
-		  var row = {};
-		 
-		  // Find all of the table cells on this row.
-		  $(this).find('td').each(function(i) {
-			// Determine the cell's column name by comparing its index
-			//  within the row with the columns list we built previously.
-			var rowName = columns[i];
-		 
-			// Add a new property to the row object, using this cell's
-			//  column name as the key and the cell's text as the value.
-			row[rowName] = $(this).text();
-		  });
-		 
-		  // Finally, return the row's object representation, to be included
-		  //  in the array that $.map() ultimately returns.
-		  return row;
-		 
-		// Don't forget .get() to convert the jQuery set to a regular array.
-		}).get();
-		
-		
-		
-		jsonTable = JSON.stringify(tableObject);
-		alert("tableObject:" + jsonTable);
-		
-		
-		alert(data);
+		event.preventDefault()
+        alert("form submit")
+		data = $('#myForm').serialize()
+		alert("sending" + data);
 		$.ajax({
              type: 'POST',
-			 url:'/lessons',
-             data: jsonTable,
+			 url: 'http://localhost:48080/lessons',
+             data: data,
 			 dataType: 'json',
-			 contentType:'application/json; charset=utf-8',
-             success: function(response) {
-				alert("Success" + response)
+             success: function(data) {
+				alert("success")
              },error: function(response){
-				alert("Error with json post" + response);
-			 
-			}
+				alert("Error with json post" + JSON.stringify(response));
+			 }     
 		});
 		return false;
 		
--- lessons.py
+++ lessons.py
@@ -9,7 +9,6 @@ from google.appengine.ext import ndb
 from datetime import datetime
 import sys
 
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -17,20 +16,18 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 	
 class LessonsPage(webapp2.RequestHandler):
 	def get(self):
+		
 		#delete the values
 		models.Lesson.deleteAllLessons()
-		#insert
-		city = 'Eau Claire'
-		date = datetime.now() 
-		location = 'testlocation'
-		cost = 26.0
-		lessons = models.Lesson.insertLesson(city, date, location, cost)
-		
 		
 		#get all lessons
-		lessons = models.Lesson.getNLessons(1)
+		lessons = models.Lesson.getNLessons(3)
 		
 		#get lesson by city
+		
+		
+		
+		
 		
 		template_values ={'lessons':lessons}
 		template = JINJA_ENVIRONMENT.get_template('templates/lessons.html')
@@ -38,10 +35,16 @@ class LessonsPage(webapp2.RequestHandler):
 		
 	
 	def post(self):
-		jsonstring = self.request.body
-		self.response.out.write(jsonstring)
-		jsonObject = json.loads(jsonstring)
-		#self.redirect('/lessons')
+		self.response.write(request.ajax)
+		self.response.write(request.args)
+		#insert
+		#self.response.write(data)
+		#city = 'Eau Claire'
+		#date = datetime.now() 
+		#location = 'testlocation'
+		#cost = 26.0
+		#models.Lesson.insertLesson(city, date, location, cost)
+		self.redirect('/lessons')
 		
 
 app = webapp2.WSGIApplication([
